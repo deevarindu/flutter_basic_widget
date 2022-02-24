5e2771f6ec09573e824c496c66e3fca0c4b2cdbc
deevarindu <deevarindu@gmail.com>
2022-02-24 19:03:47 +0700

    add scroll view

--- lib/main.dart
+++ lib/main.dart
@@ -19,207 +19,210 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('MyApp'),
         ),
-        body: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  margin: EdgeInsets.all(15),
-                  child: const Text('BERITA TERBARU',
-                      style:
-                          TextStyle(fontSize: 13, fontWeight: FontWeight.w600)),
-                ),
-                SizedBox(
-                  width: 40,
-                ),
-                Container(
-                  margin: EdgeInsets.all(15),
-                  child: const Text('PERTANDINGAN HARI INI',
-                      style:
-                          TextStyle(fontSize: 13, fontWeight: FontWeight.w600)),
-                ),
-              ],
-            ),
-            Container(
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.purple.shade300),
-                color: Colors.purple.shade200,
-              ),
-              margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 0),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.center,
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Image.asset("assets/images/img1.jpg",
-                            fit: BoxFit.fitWidth),
-                      ),
-                    ],
+                  Container(
+                    margin: EdgeInsets.all(15),
+                    child: const Text('BERITA TERBARU',
+                        style: TextStyle(
+                            fontSize: 13, fontWeight: FontWeight.w600)),
+                  ),
+                  SizedBox(
+                    width: 40,
                   ),
                   Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
+                    margin: EdgeInsets.all(15),
+                    child: const Text('PERTANDINGAN HARI INI',
+                        style: TextStyle(
+                            fontSize: 13, fontWeight: FontWeight.w600)),
+                  ),
+                ],
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.purple.shade300),
+                  color: Colors.purple.shade200,
+                ),
+                margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Image.asset("assets/images/img1.jpg",
+                              fit: BoxFit.fitWidth),
+                        ),
+                      ],
                     ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            margin: const EdgeInsets.all(10),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text('Costa Mendekat Ke Palmeiras',
+                                    style: TextStyle(
+                                        fontSize: 17,
+                                        fontWeight: FontWeight.w600)),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.all(10),
+                          child:
+                              Text('Transfer', style: TextStyle(fontSize: 15)),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey),
+                  color: Colors.white,
+                ),
+                child: Column(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey),
+                        color: Colors.white,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Colors.grey),
+                            ),
+                            child: Expanded(
+                              child: Image.asset(
+                                "assets/images/img2.jpg",
+                                fit: BoxFit.contain,
+                                height: 110,
+                                width: 195,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.symmetric(
+                                horizontal: 35, vertical: 0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text('Pique Bilang Wasit Untungkan',
+                                    style: TextStyle(fontSize: 15)),
+                                Text('Madrid, Koeman Tepok Jidat',
+                                    style: TextStyle(fontSize: 15)),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
                       children: [
                         Container(
                           margin: const EdgeInsets.all(10),
                           child: Column(
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: [
-                              Text('Costa Mendekat Ke Palmeiras',
-                                  style: TextStyle(
-                                      fontSize: 17,
-                                      fontWeight: FontWeight.w600)),
+                              Text('Barcelona Feb 13, 2021',
+                                  style: TextStyle(fontSize: 15)),
                             ],
                           ),
                         ),
                       ],
                     ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.all(10),
-                        child: Text('Transfer', style: TextStyle(fontSize: 15)),
-                      )
-                    ],
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            Container(
-              margin: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey),
-                color: Colors.white,
-              ),
-              child: Column(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.grey),
-                      color: Colors.white,
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 10, vertical: 0),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey),
+                  color: Colors.white,
+                ),
+                child: Column(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey),
+                        color: Colors.white,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Colors.grey),
+                            ),
+                            child: Expanded(
+                              child: Image.asset(
+                                "assets/images/img2.jpg",
+                                fit: BoxFit.contain,
+                                height: 110,
+                                width: 195,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.symmetric(
+                                horizontal: 35, vertical: 0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text('Pique Bilang Wasit Untungkan',
+                                    style: TextStyle(fontSize: 15)),
+                                Text('Madrid, Koeman Tepok Jidat',
+                                    style: TextStyle(fontSize: 15)),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                    child: Row(
+                    Row(
                       mainAxisAlignment: MainAxisAlignment.start,
                       children: [
                         Container(
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.grey),
-                          ),
-                          child: Expanded(
-                            child: Image.asset(
-                              "assets/images/img2.jpg",
-                              fit: BoxFit.contain,
-                              height: 110,
-                              width: 195,
-                            ),
-                          ),
-                        ),
-                        Container(
-                          margin:
-                              EdgeInsets.symmetric(horizontal: 35, vertical: 0),
+                          margin: const EdgeInsets.all(10),
                           child: Column(
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: [
-                              Text('Pique Bilang Wasit Untungkan',
-                                  style: TextStyle(fontSize: 15)),
-                              Text('Madrid, Koeman Tepok Jidat',
+                              Text('Barcelona Feb 13, 2021',
                                   style: TextStyle(fontSize: 15)),
                             ],
                           ),
                         ),
                       ],
                     ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.all(10),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text('Barcelona Feb 13, 2021',
-                                style: TextStyle(fontSize: 15)),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 10, vertical: 0),
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey),
-                color: Colors.white,
-              ),
-              child: Column(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.grey),
-                      color: Colors.white,
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Container(
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.grey),
-                          ),
-                          child: Expanded(
-                            child: Image.asset(
-                              "assets/images/img2.jpg",
-                              fit: BoxFit.contain,
-                              height: 110,
-                              width: 195,
-                            ),
-                          ),
-                        ),
-                        Container(
-                          margin:
-                              EdgeInsets.symmetric(horizontal: 35, vertical: 0),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text('Pique Bilang Wasit Untungkan',
-                                  style: TextStyle(fontSize: 15)),
-                              Text('Madrid, Koeman Tepok Jidat',
-                                  style: TextStyle(fontSize: 15)),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.all(10),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text('Barcelona Feb 13, 2021',
-                                style: TextStyle(fontSize: 15)),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
